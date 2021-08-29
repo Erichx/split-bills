45e6d388406d909dd5e1933597ad6a47cd6886eb
Erich Casagrande <erichfr0zen@gmail.com>
2021-08-28 21:27:04 -0300

    Basic structure of app
    just add the structure of app

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">hello!</div>;
+  return (
+    <div className="App">
+      <div className="logo"></div>
+      <div className="total"></div>
+      <div className="you-part"></div>
+      <div className="input-item"></div>
+      <div className="list">
+        <div className="list-container"></div>
+        <div className="tip-container"></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
