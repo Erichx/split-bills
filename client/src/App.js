919a3ef901622c28f36606a7921c114643deda7a
Erich Casagrande <erichfr0zen@gmail.com>
2021-09-15 01:11:02 -0300

    css fix
    now we have cool nightly colors ;)

--- client/src/App.js
+++ client/src/App.js
@@ -3,37 +3,48 @@ import "./App.scss";
 function App() {
   return (
     <div className="App">
-      <div className="logo">Logo</div>
       <div className="amount">
         <div className="total">
-          <p className="small">Total</p>
+          <label className="small">Total</label>
           <input className="total-amount" type="text" value="200.00" />
         </div>
         <div className="your-part">
-          <p className="small">your share:</p>
+          <label className="small">your share:</label>
           <p className="your-amount">110.00</p>
         </div>
       </div>
       <div className="input-item">
-        <button className="item-add" type="button">
-          +
+        <button className="item-add button-amount" type="button">
+          <i className="bi bi-plus" />
         </button>
-        <input id="item-name" className="input-basic" type="text" value="" />
+        <input
+          id="item-name"
+          className="input-basic"
+          type="text"
+          value=""
+          placeholder="Item"
+        />
 
-        <input id="item-value" className="input-basic" type="text" value="" />
+        <input
+          id="item-value"
+          className="input-basic"
+          type="text"
+          value=""
+          placeholder="0.00"
+        />
       </div>
       <div className="list">
         <div className="list-container">
           <div className="item">
             <p>item 1 50.00</p>
-            <button className="item-remove" type="button">
-              X
+            <button className="item-remove button-amount" type="button">
+              <i className="bi bi-x" />
             </button>
           </div>
           <div className="item">
             <p>item 2 50.00</p>
-            <button className="item-remove" type="button">
-              X
+            <button className="item-remove button-amount" type="button">
+              <i className="bi bi-x" />
             </button>
           </div>
         </div>
